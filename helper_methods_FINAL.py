cd7eb4fe33d3a1e619b9f0455c9e2ca8b4adc164
Steven Chiacchira <schiacchira@Gmail.com>
2024-02-13 12:37:52 -0500

    Added gitignore, removed attacks.py, updated hms

--- helper_methods_FINAL.py
+++ helper_methods_FINAL.py
@@ -688,3 +688,125 @@ def mim_whitebox_eval(model,
     err_mim = (model(X_mim).data.max(1)[1] != y.data).float().sum()
 
     return err, err_mim
+
+
+def cw_whitebox(model,
+                 X,
+                 y,
+                 device,
+                 dataset,
+                 epsilon=args.epsilon,
+                 num_steps=args.num_steps,
+                 step_size=args.step_size):
+    """
+        Attacks the specified image X using the CW attack and returns the adversarial example
+
+        :param model: model being attacked
+        :param X: image being attacked
+        :param y: correct label of the image being attacked
+        :param device: current device
+        :param epsilon: epsilon size for the PGD attack
+        :param num_steps: number of perturbation steps
+        :param step_size: step size of perturbation steps
+
+        :return adversarial example found with the CW attack
+    """
+
+    # Create X_pgd basis by duplicating X as a variable
+    X_pgd = Variable(X.data, requires_grad=True)
+
+    # If adding random, create random noice between - and + epsilon and add to X_pgd
+    if args.random:
+        random_noise = torch.FloatTensor(*X_pgd.shape).uniform_(-epsilon, epsilon).to(device)
+        X_pgd = Variable(X_pgd.data + random_noise, requires_grad=True)
+
+    # For each perturbation step:
+    for _ in range(num_steps):
+        # Create the SGD optimizer and zero the gradients
+        opt = optim.SGD([X_pgd], lr=1e-3)
+        opt.zero_grad()
+
+        # With gradients, set up the CW loss and step backward
+        with torch.enable_grad():
+            loss = CWLoss(100 if dataset == 'cifar100' else 10)(model(X_pgd), y)
+        loss.backward()
+
+        # Calculate the perturbation eta as the step size in the gradient direction of X_pgd
+        eta = step_size * X_pgd.grad.data.sign()
+
+        # Add the perturbation to X_pgd
+        X_pgd = Variable(X_pgd.data + eta, requires_grad=True)
+
+        # Set the perturbation to the difference between X and X_adv, clamped by +/- epsilon
+        eta = torch.clamp(X_pgd.data - X.data, -epsilon, epsilon)
+
+        # Add the new perturbation to X_pgd again
+        X_pgd = Variable(X.data + eta, requires_grad=True)
+
+        # Clamp X_gd to be between 0 and 1, as a Variable
+        X_pgd = Variable(torch.clamp(X_pgd, 0, 1.0), requires_grad=True)
+    return X_pgd
+
+def mim_whitebox(model,
+                  X,
+                  y,
+                  device,
+                  epsilon=args.epsilon,
+                  num_steps=args.num_steps,
+                  step_size=args.step_size,
+                  decay_factor=1.0):
+    """
+        Attacks the specified image X using the MIM attack and returns the adversarial example
+
+        :param model: model being attacked
+        :param X: image being attacked
+        :param y: correct label of the image being attacked
+        :param device: current device
+        :param epsilon: epsilon size for the PGD attack
+        :param num_steps: number of perturbation steps
+        :param step_size: step size of perturbation steps
+        :param decay_factor: factor of decay for gradients
+
+        :return adversarial example found with the MIM attack
+    """
+
+    # Create X_pgd basis by duplicating X as a variable
+    X_pgd = Variable(X.data, requires_grad=True)
+
+    # If adding random, create random noice between - and + epsilon and add to X_pgd
+    if args.random:
+        random_noise = torch.FloatTensor(*X_pgd.shape).uniform_(-epsilon, epsilon).to(device)
+        X_pgd = Variable(X_pgd.data + random_noise, requires_grad=True)
+
+    # Set the previous gradient to a tensor of 0s in the shape of X
+    previous_grad = torch.zeros_like(X.data)
+
+    # For each perturbation step:
+    for _ in range(num_steps):
+        # Create the SGD optimizer and zero the gradients
+        opt = optim.SGD([X_pgd], lr=1e-3)
+        opt.zero_grad()
+
+        # With gradients, set up the cross-entropy loss and step backward
+        with torch.enable_grad():
+            loss = nn.CrossEntropyLoss()(model(X_pgd), y)
+        loss.backward()
+
+        # Calculate the gradient by dividing it by the average
+        grad = X_pgd.grad.data / torch.mean(torch.abs(X_pgd.grad.data), [1, 2, 3], keepdim=True)
+
+        # Calculate the previous gradient by multiplying it by the decay factor and adding to the grad
+        previous_grad = decay_factor * previous_grad + grad
+
+        # Perturb X_pgd in the direction of the previous grad, by the step size
+        X_pgd = Variable(X_pgd.data + step_size * previous_grad.sign(), requires_grad=True)
+
+        # Set the perturbation to the difference between X and X_adv, clamped by +/- epsilon
+        eta = torch.clamp(X_pgd.data - X.data, -epsilon, epsilon)
+
+        # Add the new perturbation to X_pgd againt
+        X_pgd = Variable(X.data + eta, requires_grad=True)
+
+        # Clamp X_gd to be between 0 and 1, as a Variable
+        X_pgd = Variable(torch.clamp(X_pgd, 0, 1.0), requires_grad=True)
+    return X_pgd
